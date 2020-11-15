0a2a67eec36d885bc115596f61075dbba79e2464
m0ric <andreicristian6@protonmail.com>
2020-11-15 13:02:43 +0200

    Add test for mail model

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 import unittest
 
-class TestSum(unittest.TestCase):
+import MailPrototype
 
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+from Models import Mail
 
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1,2,3)), 6, "Should be 6")
+class TestMail(unittest.TestCase):
+
+    def test_mail_class(self):
+        _mail = Mail()
+        _mail.setFrom("test@test.com")
+        _mail.setTo("tester@test.com")
+        _mail.setSubject("TEST")
+        _mail.setBody("This is just a test.")
+
+        self.assertEqual(_mail.getFrom, "test@test.com",
+        "Mail._from should be \'test@test.com\'")
+        self.assertEqual(_mail.getTo, "tester@test.com",
+        "Mail._to should be \'tester@test.com\'")
+        self.assertEqual(_mail.getSubject, "TEST",
+        "Mail._subject should be \'TEST\'")
+        self.assertEqual(_mail.getBody, "This is just a test.",
+        "Mail._body should be \'This is just a test.\'")
 
 if __name__ == '__main__':
     unittest.main()
