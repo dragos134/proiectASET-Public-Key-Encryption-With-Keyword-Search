180d190ee2c94db06eb3269aaac9e8df1bed165e
m0ric <andreicristian6@protonmail.com>
2020-11-15 13:09:46 +0200

    Implement setters and getters for mail model

--- Models.py
+++ Models.py
@@ -3,3 +3,27 @@ class Mail:
     _to         = None
     _subject    = None
     _body       = None
+
+    def setFrom(self, _from):
+        self._from = _from
+
+    def setTo(self, to):
+        self._to = to
+
+    def setSubject(self, subject):
+        self._subject = subject
+
+    def setBody(self, body):
+        self._body = body
+
+    def getFrom(self):
+        return self._from
+
+    def getTo(self):
+        return self._to
+
+    def getSubject(self):
+        return self._subject
+
+    def getBody(self):
+        return self._body
