07220e95464639b1012629fb02396e76d5962ac3
m0ric <andreicristian6@protonmail.com>
2020-11-15 13:08:58 +0200

    Fixed typo in test.py

--- test.py
+++ test.py
@@ -13,13 +13,13 @@ class TestMail(unittest.TestCase):
         _mail.setSubject("TEST")
         _mail.setBody("This is just a test.")
 
-        self.assertEqual(_mail.getFrom, "test@test.com",
+        self.assertEqual(_mail.getFrom(), "test@test.com",
         "Mail._from should be \'test@test.com\'")
-        self.assertEqual(_mail.getTo, "tester@test.com",
+        self.assertEqual(_mail.getTo(), "tester@test.com",
         "Mail._to should be \'tester@test.com\'")
-        self.assertEqual(_mail.getSubject, "TEST",
+        self.assertEqual(_mail.getSubject(), "TEST",
         "Mail._subject should be \'TEST\'")
-        self.assertEqual(_mail.getBody, "This is just a test.",
+        self.assertEqual(_mail.getBody(), "This is just a test.",
         "Mail._body should be \'This is just a test.\'")
 
 if __name__ == '__main__':
