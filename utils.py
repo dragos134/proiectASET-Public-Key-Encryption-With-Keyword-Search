1aa41a8eec370b268d56b9f51d8b6d8ff5e3a7e3
m0ric <andreicristian6@protonmail.com>
2020-11-15 13:57:45 +0200

    Fix hash function failing test.

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ import hashlib
 
 def hash(plaintext):
     _salt = "pkekspkekspkekspkekspkekspkekspkekspkeks00000000000000000000000000"
-    hr = hashlib.scrypt(plaintext.encode("utf-8"), _salt.encode("utf-8"),
-    16384, 8, 1)
-    return hr
+    hr = hashlib.scrypt(plaintext.encode("utf-8"), salt = _salt.encode("utf-8"),
+    n = 16384, r = 8, p = 1)
+    return hr.hex()
